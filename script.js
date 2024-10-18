9c6bf1a45dede05a9f6259ace612cc56d5be9276
luka <spammluka@gmail.com>
2024-10-18 20:24:45 +0400

    finished

--- script.js
+++ script.js
@@ -7,5 +7,30 @@ function AddTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (
+        e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+}, false);
+function saveData(){
+    localStorage.setItem("data, lostConatainer.innerHTML");
+
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
